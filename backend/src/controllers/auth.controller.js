584c99a1cf0ad533a1bf16fb3f4c975fb87ebf11
PRABANJAN-A182005 <prabanjan.offical@gmail.com>
2026-01-11 19:38:44 +0530

    implement login functionality with validation and error handling; update routes to include login and logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -59,3 +59,36 @@ export const signup = async(req, res) => {
         res.status(500).json({ message: "Server error" });
     }
 }
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        if(!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        const user = await User.findOne({ email });
+        if(!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        generateToken(user._id, res);
+        res.status(200).json({
+            message: "Login successful",
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePicture: user.profilePicture
+        });
+    } catch (error) {
+        console.error("Login error:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+export const logout = async (_, res) => {
+    res.cookie('jwt', '', { maxAge: 0 });
+    res.status(200).json({ message: "Logout successful" });
+};
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { ENV } from './env';
+import { ENV } from './env.js';
 export const connectDB = async (mongoURL) => {
     try {
         const {MONGODB_URL} = ENV;
